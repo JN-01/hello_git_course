99b995cbdff4d0698d5551155c23b3f1bffdeca9
Joni <joni.p.nieminen@tuni.fi>
2025-03-19 10:24:29 +0200

    Revert "Merge-sort"
    This reverts commit dd8c56cede9b109c1df7bda13abc1b1b7b79ed0b.
    
    Haettu vanha versio mergesortista, jossa on debug tulostukset

--- origin/basics/mergesort.py
+++ origin/basics/mergesort.py
@@ -8,6 +8,7 @@ def debug_print(debug_msg=None, **kwargs):
         print("{}: {}".format(key, value))
 
 
+<<<<<<< HEAD:origin/basics/mergesort.py
 def mergesort(array):
     if len(array) <= 1:
         return array
@@ -37,6 +38,8 @@ def merge(left, right):
     return merged
 
 
+=======
+>>>>>>> parent of dd8c56c (Merge-sort):basics/mergesort.py
 if __name__ == "__main__":
     input_str = input("Enter numbers, separated by ',': ")
     input_list = input_str.split(",")
@@ -48,5 +51,9 @@ if __name__ == "__main__":
             print("Invalid input.")
             quit(1)
 
+<<<<<<< HEAD:origin/basics/mergesort.py
     sorted_list = mergesort(value_list)
     print(sorted_list)
+=======
+    debug_print(value_list=value_list)
+>>>>>>> parent of dd8c56c (Merge-sort):basics/mergesort.py
